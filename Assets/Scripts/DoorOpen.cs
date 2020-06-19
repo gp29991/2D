18a032b50ed5842229d5c0b94f0e1342423f497e
gp29991 <61902556+gp29991@users.noreply.github.com>
2020-06-19 19:10:59 +0200

    Initial commit

---
+++ Assets/Scripts/DoorOpen.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorOpen : MonoBehaviour
+{
+    public SpriteRenderer door;
+    public Sprite doorOpen;
+    public Sprite mySprite;
+    public SpriteRenderer toggle;
+    public Vector3 position;
+    public bool moved = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        toggle = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            toggle.sprite = mySprite;
+            door.sprite = doorOpen;
+            if (!moved)
+            {
+                position = transform.localPosition;
+                position.y -= 0.07f;
+                position.x -= 0.09f;
+                transform.localPosition = position;
+                moved = true;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/EndLevel.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndLevel : MonoBehaviour
+{
+    public SpriteRenderer check;
+    public Sprite openDoorSprite;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        check = GetComponent<SpriteRenderer>();
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player" && check.sprite == openDoorSprite)
+        {
+            SceneManager.LoadScene("LevelEnd");
+        }
+    }
+}
